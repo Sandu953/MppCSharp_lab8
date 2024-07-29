3731e48fff232fea2cce4b38d4f1f73ffae703ae
Pop Alexandru Ionut <134011559+Sandu953@users.noreply.github.com>
2024-07-29 23:28:15 +0300

    Server

---
+++ server/ServiceImpl.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using lab8.Repository;
+using lab8.Domain;
+using services;
+
+
+
+namespace server
+{
+    public class ServicesImpl : IServices
+    {
+        private AgentieRepository agentieRepo;
+        private ExcursieRepository excursieRepo;
+        private RezervareRepository rezervareRepo;
+        private ConcurrentDictionary<long, IObserver> loggedClients;
+
+        public ServicesImpl(AgentieRepository agentieRepo, ExcursieRepository excursieRepo, RezervareRepository rezervareRepo)
+        {
+            this.agentieRepo = agentieRepo;
+            this.excursieRepo = excursieRepo;
+            this.rezervareRepo = rezervareRepo;
+            loggedClients = new ConcurrentDictionary<long, IObserver>();
+        }
+
+        public bool HandleLogin(string username, string password, IObserver client)
+        {
+            Agentie agentie = agentieRepo.FindBy(username, password);
+            if (agentie != null)
+            {
+                if (loggedClients.ContainsKey(agentie.Id))
+                    return false;
+
+                loggedClients.TryAdd(agentie.Id, client);
+            }
+            else
+            {
+                return false;
+            }
+
+            return agentieRepo.loginByUsernamePassword(username, password);
+        }
+
+        public long GetId(string username, string password)
+        {
+            Agentie ag = agentieRepo.FindBy(username, password);
+            return ag.Id;
+        }
+
+        public void Logout(Agentie user, IObserver client)
+        {
+            Agentie ag = agentieRepo.FindByUser(user.Username);
+            if (ag == null)
+                throw new Exception("User " + user.Id + " is not logged in.");
+
+            if (!loggedClients.TryRemove(ag.Id, out IObserver localClient))
+                throw new Exception("User " + user.Id + " is not logged in.");
+        }
+
+        public IEnumerable<Excursie> GetAllExcursii()
+        {
+            return excursieRepo.FindAll();
+        }
+
+        public IEnumerable<Excursie> GetExcursiiBetweenHours(string obiectiv, string ora1, string ora2)
+        {
+            Console.WriteLine("ServiceImpl: " + obiectiv + " " + ora1 + " " + ora2);
+            return excursieRepo.findBeetwenHours(ora1, ora2, obiectiv);
+        }
+
+        public int GetFreeSeats(int id)
+        {
+            return excursieRepo.findLocuriLibere(id);
+        }
+
+        public void AddRezervare(long id, long ex, string nume, string telefon, int bilet)
+        {
+            try
+            {
+                Rezervare rez = new Rezervare(id,ex, nume, telefon, bilet);
+                rezervareRepo.Save(rez);
+                Console.WriteLine("Notifying");
+                NotifyRezervare(id);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private const int DefaultThreadsNo = 5;
+
+        private void NotifyRezervare(long id)
+        {
+            var tasks = new List<Task>();
+            foreach (var entry in loggedClients)
+            {
+                Console.WriteLine("AAA" + entry.Key);
+                if (entry.Key != id)
+                {
+                    IObserver chatClient = entry.Value;
+                    if (chatClient != null)
+                    {
+                        tasks.Add(Task.Run(async () =>
+                        {
+                            try
+                            {
+                                Task.Run(() => chatClient.ReservationMade());
+                            }
+                            catch (Exception e)
+                            {
+                                Console.Error.WriteLine("Error notifying " + e);
+                            }
+                        }));
+                    }
+                }
+            }
+            Task.WhenAll(tasks);
+        }
+
+       
+    }
+}
---
+++ server/StartRpcServer.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using lab8.Repository;
+using server;
+using services;
+using network;
+using System.Net.Sockets;
+using networking;
+
+namespace server 
+{
+    class StartRpcServer
+    {
+        private static int defaultPort = 55555;
+
+        static void Main(string[] args)
+        {
+            // UserRepository agentieRepo = new UserRepositoryMock();
+            //Dictionary<string, string> serverProps = new Dictionary<string, string>();
+            //try
+            //{
+            //    foreach (var line in File.ReadLines("C:\\Users\\probook\\source\\repos\\server\\chatserver.properties"))
+            //    {
+            //        var parts = line.Split('=');
+            //        serverProps[parts[0]] = parts[1];
+            //    }
+            //    Console.WriteLine("Server properties set.");
+            //    foreach (var prop in serverProps)
+            //    {
+            //        Console.WriteLine($"{prop.Key}: {prop.Value}");
+            //    }
+            //}
+            //catch (FileNotFoundException e)
+            //{
+            //    Console.WriteLine("Cannot find chatserver.properties " + e);
+            //    return;
+            //}
+            
+
+            AgentieRepository agentieRepo = new AgentieRepository("Data Source=C:/Users/probook/Desktop/EXAMEN MAP/mpp-proiect-java-Sandu953/FirmaExcursie.db;Version=3;");
+            //AgentieRepository agentieORMRepo = new AgentieRepository(new AppDbContext());
+            ExcursieRepository excursieRepo = new ExcursieRepository("Data Source=C:/Users/probook/Desktop/EXAMEN MAP/mpp-proiect-java-Sandu953/FirmaExcursie.db;Version=3;");
+            RezervareRepository rezervareRepo = new RezervareRepository("Data Source=C:/Users/probook/Desktop/EXAMEN MAP/mpp-proiect-java-Sandu953/FirmaExcursie.db;Version=3;", excursieRepo);
+            IServices serverImpl = new ServicesImpl(agentieRepo, excursieRepo, rezervareRepo);
+
+            int serverPort = defaultPort;
+            //if (!int.TryParse(serverProps["chat.server.port"], out serverPort))
+            //{
+            //    Console.WriteLine("Wrong Port Number");
+            //    Console.WriteLine("Using default port " + defaultPort);
+            //    serverPort = defaultPort;
+            //}
+
+            Console.WriteLine("Starting server on port: " + serverPort);
+            //SerialProjectServer server = new SerialProjectServer( serverImpl);
+            // ProjectServerProxy serverProxy = new ProjectServerProxy()
+            RPCConcurrentServerProto serverProxy = new RPCConcurrentServerProto("127.0.0.1", 55555, serverImpl);
+            serverProxy.Start();
+            Console.ReadLine();
+
+            //using (RpcConcurrentServer server = new RpcConcurrentServer(serverPort, serverImpl))
+            //{
+            //    try
+            //    {
+            //        server.Start();
+            //        Task.Delay(-1).Wait(); // Keep the server running until manually stopped
+            //    }
+            //    catch (ServerException e)
+            //    {
+            //        Console.WriteLine("Error starting the server" + e.Message);
+            //    }
+            //    finally
+            //    {
+            //        try
+            //        {
+            //            server.Stop();
+            //        }
+            //        catch (ServerException e)
+            //        {
+            //            Console.WriteLine("Error stopping server " + e.Message);
+            //        }
+            //    }
+            //}
+            //using (ConcurrentServerProto server = new ConcurrentServerProto(serverPort, serverImpl))
+            //{
+            //    try
+            //    {
+            //        server.Start();
+            //        Task.Delay(-1).Wait(); // Keep the server running until manually stopped
+            //    }
+            //    catch (ServerException e)
+            //    {
+            //        Console.WriteLine("Error starting the server" + e.Message);
+            //    }
+            //    finally
+            //    {
+            //        try
+            //        {
+            //            server.Stop();
+            //        }
+            //        catch (ServerException e)
+            //        {
+            //            Console.WriteLine("Error stopping server " + e.Message);
+            //        }
+            //    }
+            //}
+        }
+
+        //public class SerialProjectServer : ConcurrentServerProto
+        //{
+        //    private IServices server;
+        //    private ProjectProtoWorker worker;
+        //    public SerialProjectServer(string host, int port, IServices server) : base(host, port)
+        //    {
+        //        this.server = server;
+        //        Console.WriteLine("SerialProjectServer...");
+        //    }
+        //    protected override Thread createWorker(TcpClient client)
+        //    {
+        //        worker = new ProjectProtoWorker(server, client);
+        //        return new Thread(new ThreadStart(worker.run));
+        //    }
+        //}
+    }
+}
