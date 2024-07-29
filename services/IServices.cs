d5a8511982f542422161baf996ea8aee820e5989
Pop Alexandru Ionut <134011559+Sandu953@users.noreply.github.com>
2024-07-29 23:31:52 +0300

    Services

---
+++ services/IServices.cs
@@ -0,0 +1,20 @@
+﻿using lab8.Domain;
+using services;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace services 
+{
+    public interface IServices
+    {
+        bool HandleLogin(string username, string password, IObserver client);
+        void Logout(Agentie user, IObserver client);
+        IEnumerable<Excursie> GetAllExcursii();
+        IEnumerable<Excursie> GetExcursiiBetweenHours(string obiectiv, string ora1, string ora2);
+        int GetFreeSeats(int id);
+        void AddRezervare(long id, long ex, string nume, string telefon, int bilet);
+        long GetId(string username, string password);
+    }
+}
